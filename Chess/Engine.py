d050583e11820f682cfb81a20e9e2f28d2e316ef
Austin <medinajaustin@gmail.com>
2021-03-26 10:41:52 -0400

    Added simple moving of pieces

--- Chess/Engine.py
+++ Chess/Engine.py
@@ -24,6 +24,50 @@ class GameState():
 
             self.whiteMove = True
             self.moveLog = []
-
             
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        
+        self.moveLog.append(move) 
+        
+        # Switch to other player's turn
+        self.whiteToMove = not self.whiteMove
+
+
+#
+# Class to control and make moves in the GameState
+#
+class Move():
+    
+    # Dictionary to convert rows into chess ranks and columns into files
+    rankToRow = {"1": 7, "2": 6, "3": 5, "4": 4, 
+                    "5": 3, "6": 2, "7": 1, "8": 0 }
+    
+    rowToRank = {v: k for k, v in rankToRow.items()}
+    
+    
+    fileToCol = {"a": 0, "b": 1, "c": 2, "d": 3, 
+                    "e": 4, "f": 5, "g": 6, "h": 7 }
+    
+    colToFile = {v: k for k, v in fileToCol.items()}
+        
+    def __init__(self, start, end, board):
+                
+        self.startRow = start[0]
+        self.startCol = start[1]
+        self.endRow = end[0]
+        self.endCol = end[1]
+        
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+        
+        
+        
+                
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+        
+    def getRankFile(self, row, col):
+        return self.colToFile[col] + self.rowToRank[row]
 
--- Chess/Main.py
+++ Chess/Main.py
@@ -25,7 +25,7 @@ def loadImages():
     pieces = ['wP', 'wR', 'wN', 'wB', 'wQ', 'wK', 'bP', 'bR', 'bN', 'bB', 'bQ', 'bK']
     
     for piece in pieces:
-        images[piece] = p.transform.scale(p.image.load("../images/" + piece + ".png"), (squareSize, squareSize))
+        images[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (squareSize, squareSize))
     
 
 
@@ -33,6 +33,8 @@ def loadImages():
 # User Input
 # Graphics Handler
 def main():
+   
+    p.init()
     
     screen = p.display.set_mode((width, height))
     clock = p.time.Clock()
@@ -41,12 +43,38 @@ def main():
     gs = Engine.GameState()
     loadImages()
     
+    # Holds user's last clicked square
+    selected = ()
+    
+    # Keep track of player's clicks
+    playerClick = []
+    
     running = True
     while running:
         for event in p.event.get():
             if event.type == p.QUIT:
                 running = False
+            elif event.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()
+                col = location[0] // squareSize
+                row = location[1] // squareSize
         
+                # Check if square was already selected, if yes deselect it
+                if selected == (row, col):
+                    selected = ()
+                    playerClick = []
+                else:                
+                    selected = (row, col)
+                    playerClick.append(selected)
+                    
+                # Check if two location are selected    
+                if len(playerClick) == 2:
+                    move = Engine.Move(playerClick[0], playerClick[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    selected = ()
+                    playerClick = []
+                    
         drawGameState(screen, gs)        
         clock.tick(fps)
         p.display.flip()
